755a3f59e923fcdfe9c53aa1233eb07b13878c02
adarshtsatheesh <adarshtsatheesh@gmail.com>
2024-04-01 18:18:47 +0530

    decap cms try

--- js/script.js
+++ js/script.js
@@ -94,3 +94,10 @@ function toggleContent(content) {
 toggleContent('skill');
 
 
+
+// cms
+import CMS from "decap-cms-app";
+// Initialize the CMS object
+CMS.init();
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate("my-template", MyTemplate);

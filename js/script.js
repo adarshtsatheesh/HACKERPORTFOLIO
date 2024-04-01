e1090f6f37acf5b83586ab6b5494d4eef34df3a4
adarshtsatheesh <adarshtsatheesh@gmail.com>
2024-04-01 18:23:04 +0530

    cms remove

--- js/script.js
+++ js/script.js
@@ -95,9 +95,3 @@ toggleContent('skill');
 
 
 
-// cms
-import CMS from "decap-cms-app";
-// Initialize the CMS object
-CMS.init();
-// Now the registry is available via the CMS object.
-CMS.registerPreviewTemplate("my-template", MyTemplate);
